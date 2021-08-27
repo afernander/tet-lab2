782592ad7be9946331907f4487787d471f67deef
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2021-08-27 11:40:12 -0500

    404 handler

--- server1/http_server.py
+++ server1/http_server.py
@@ -1,23 +1,31 @@
 # Python 3 server example
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.parse import parse_qs
+from urllib.parse import urlparse, parse_qs
 import constants
 
 class TableServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        query = parse_qs(self.path)
+        url = urlparse(self.path)
+        path = url.path
+        query = parse_qs(url.query)
         
-        if not bool(query):
-            print(query)
-            self.send_response(400)
-            self.send_header("Content-Type", "text/plain")
-            self.end_headers()
-            self.wfile.write(bytes("No params detected.", "utf-8"))
+        if (path == '/'):
+            if not bool(query):
+                print(query)
+                self.send_response(400)
+                self.send_header("content-type", "text/plain")
+                self.end_headers()
+                self.wfile.write(bytes("No params detected.", "utf-8"))
+            else:
+                self.send_response(200)
+                self.send_header("content-type", "text/plain")
+                self.end_headers()
+                self.wfile.write(bytes("This is an example web server.", "utf-8"))
         else:
-            self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
+            self.send_response(404)
+            self.send_header("content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(bytes("This is an example web server.", "utf-8"))
+            self.wfile.write(bytes("404 Error: Resource %s not found" % path, "utf-8"))
 
 if __name__ == "__main__":
     webServer = HTTPServer((constants.IP_SERVER, constants.PORT), TableServer)
