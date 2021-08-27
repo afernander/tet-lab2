d038469493ad44af84b2f002872ed63dd2536de7
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2021-08-27 11:14:35 -0500

    parsing query string example

--- server1/http_server.py
+++ server1/http_server.py
@@ -1,13 +1,23 @@
 # Python 3 server example
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
 import constants
 
 class TableServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(bytes("This is an example web server.", "utf-8"))
+        query = parse_qs(self.path)
+        
+        if not bool(query):
+            print(query)
+            self.send_response(400)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(bytes("No params detected.", "utf-8"))
+        else:
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(bytes("This is an example web server.", "utf-8"))
 
 if __name__ == "__main__":
     webServer = HTTPServer((constants.IP_SERVER, constants.PORT), TableServer)
